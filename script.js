946294ca6a2210ec1ff719cccdd6dd9eadd7792f
Dmasbs <dashaandreva1998@gmail.com>
2023-04-30 22:56:12 +0300

    feat: add css and array object and create title, textarea

---
+++ script.js
@@ -0,0 +1,541 @@
+const ARR_RU = [
+  [
+    {
+      code: 'Backquote',
+      ru: {
+        default: 'ё',
+        shift: 'Ё',
+        caps: 'Ё',
+      },
+    },
+    {
+      code: 'Digit1',
+      ru: {
+        default: '1',
+        shift: '!',
+        caps: '1',
+      },
+    },
+    {
+      code: 'Digit2',
+      ru: {
+        default: '2',
+        shift: '"',
+        caps: '2',
+      },
+    },
+    {
+      code: 'Digit3',
+      ru: {
+        default: '3',
+        shift: '№',
+        caps: '3',
+      },
+    },
+    {
+      code: 'Digit4',
+      ru: {
+        default: '4',
+        shift: ';',
+        caps: '4',
+      },
+    },
+    {
+      code: 'Digit5',
+      ru: {
+        default: '5',
+        shift: '%',
+        caps: '5',
+      },
+    },
+    {
+      code: 'Digit6',
+      ru: {
+        default: '6',
+        shift: ':',
+        caps: '6',
+      },
+    },
+    {
+      code: 'Digit7',
+      ru: {
+        default: '7',
+        shift: '?',
+        caps: '7',
+      },
+    },
+    {
+      code: 'Digit8',
+      ru: {
+        default: '8',
+        shift: '*',
+        caps: '8',
+      },
+    },
+    {
+      code: 'Digit9',
+      ru: {
+        default: '9',
+        shift: '(',
+        caps: '9',
+      },
+    },
+    {
+      code: 'Digit0',
+      ru: {
+        default: '0',
+        shift: ')',
+        caps: '0',
+      },
+    },
+    {
+      code: 'Minus',
+      ru: {
+        default: '-',
+        shift: '_',
+        caps: '-',
+      },
+    },
+    {
+      code: 'Equal',
+      ru: {
+        default: '=',
+        shift: '+',
+        caps: '=',
+      },
+    },
+    {
+      code: 'Backspace',
+      ru: {
+        default: 'delete',
+        shift: 'delete',
+        caps: 'delete',
+      },
+    },
+  ],
+  [
+    {
+      code: 'Tab',
+      ru: {
+        default: 'tab',
+        shift: 'tab',
+        caps: 'tab',
+      },
+    },
+    {
+      code: 'KeyQ',
+      ru: {
+        default: 'й',
+        shift: 'Й',
+        caps: 'Й',
+      },
+    },
+    {
+      code: 'KeyW',
+      ru: {
+        default: 'ц',
+        shift: 'Ц',
+        caps: 'Ц',
+      },
+    },
+    {
+      code: 'KeyE',
+      ru: {
+        default: 'у',
+        shift: 'У',
+        caps: 'У',
+      },
+    },
+    {
+      code: 'KeyR',
+      ru: {
+        default: 'к',
+        shift: 'К',
+        caps: 'К',
+      },
+    },
+    {
+      code: 'KeyT',
+      ru: {
+        default: 'е',
+        shift: 'Е',
+        caps: 'Е',
+      },
+    },
+    {
+      code: 'KeyY',
+      ru: {
+        default: 'н',
+        shift: 'Н',
+        caps: 'Н',
+      },
+    },
+    {
+      code: 'KeyU',
+      ru: {
+        default: 'г',
+        shift: 'Г',
+        caps: 'Г',
+      },
+    },
+    {
+      code: 'KeyI',
+      ru: {
+        default: 'ш',
+        shift: 'Ш',
+        caps: 'Ш',
+      },
+    },
+    {
+      code: 'KeyO',
+      ru: {
+        default: 'щ',
+        shift: 'Щ',
+        caps: 'Щ',
+      },
+    },
+    {
+      code: 'KeyP',
+      ru: {
+        default: 'з',
+        shift: 'З',
+        caps: 'З',
+      },
+    },
+    {
+      code: 'BracketLeft',
+      ru: {
+        default: 'х',
+        shift: 'Х',
+        caps: 'Х',
+      },
+    },
+    {
+      code: 'BracketRight',
+      ru: {
+        default: 'ї',
+        shift: 'Ї',
+        caps: 'Ї',
+      },
+    },
+    {
+      code: 'Backslash',
+      ru: {
+        default: '\\',
+        shift: '/',
+        caps: '\\',
+      },
+    },
+    {
+      code: 'Delete',
+      ru: {
+        default: 'Delete',
+        shift: 'Delete',
+        caps: 'Delete',
+      },
+    },
+  ],
+  [
+    {
+      code: 'CapsLock',
+      ru: {
+        default: 'caps lock',
+        shift: 'caps lock',
+        caps: 'caps lock',
+      },
+    },
+    {
+      code: 'KeyA',
+      ru: {
+        default: 'ф',
+        shift: 'Ф',
+        caps: 'Ф',
+      },
+    },
+    {
+      code: 'KeyS',
+      ru: {
+        default: 'ы',
+        shift: 'Ы',
+        caps: 'Ы',
+      },
+    },
+    {
+      code: 'KeyD',
+      ru: {
+        default: 'в',
+        shift: 'В',
+        caps: 'В',
+      },
+    },
+    {
+      code: 'KeyF',
+      ru: {
+        default: 'а',
+        shift: 'А',
+        caps: 'А',
+      },
+    },
+    {
+      code: 'KeyG',
+      ru: {
+        default: 'п',
+        shift: 'П',
+        caps: 'П',
+      },
+    },
+    {
+      code: 'KeyH',
+      ru: {
+        default: 'р',
+        shift: 'Р',
+        caps: 'Р',
+      },
+    },
+    {
+      code: 'KeyJ',
+      ru: {
+        default: 'о',
+        shift: 'О',
+        caps: 'О',
+      },
+    },
+    {
+      code: 'KeyK',
+      ru: {
+        default: 'л',
+        shift: 'Л',
+        caps: 'Л',
+      },
+    },
+    {
+      code: 'KeyL',
+      ru: {
+        default: 'д',
+        shift: 'Д',
+        caps: 'Д',
+      },
+    },
+    {
+      code: 'Semicolon',
+      ru: {
+        default: 'ж',
+        shift: 'Ж',
+        caps: 'Ж',
+      },
+    },
+    {
+      code: 'Quote',
+      ru: {
+        default: 'є',
+        shift: 'Є',
+        caps: 'Є',
+      },
+    },
+    {
+      code: 'Enter',
+      ru: {
+        default: 'return',
+        shift: 'return',
+        caps: 'return',
+      },
+    },
+  ],
+  [
+    {
+      code: 'ShiftLeft',
+      ru: {
+        default: 'shift',
+        shift: 'shift',
+        caps: 'shift',
+      },
+    },
+    {
+      code: 'KeyZ',
+      ru: {
+        default: 'я',
+        shift: 'Я',
+        caps: 'Я',
+      },
+    },
+    {
+      code: 'KeyX',
+      ru: {
+        default: 'ч',
+        shift: 'Ч',
+        caps: 'Ч',
+      },
+    },
+    {
+      code: 'KeyC',
+      ru: {
+        default: 'с',
+        shift: 'С',
+        caps: 'С',
+      },
+    },
+    {
+      code: 'KeyV',
+      ru: {
+        default: 'м',
+        shift: 'М',
+        caps: 'М',
+      },
+    },
+    {
+      code: 'KeyB',
+      ru: {
+        default: 'і',
+        shift: 'І',
+        caps: 'І',
+      },
+    },
+    {
+      code: 'KeyN',
+      ru: {
+        default: 'т',
+        shift: 'Т',
+        caps: 'Т',
+      },
+    },
+    {
+      code: 'KeyM',
+      ru: {
+        default: 'ь',
+        shift: 'Ь',
+        caps: 'Ь',
+      },
+    },
+    {
+      code: 'Comma',
+      ru: {
+        default: 'б',
+        shift: 'Б',
+        caps: 'Б',
+      },
+    },
+    {
+      code: 'Period',
+      ru: {
+        default: 'ю',
+        shift: 'Ю',
+        caps: 'Ю',
+      },
+    },
+    {
+      code: 'Slash',
+      ru: {
+        default: '.',
+        shift: ',',
+        caps: '.',
+      },
+    },
+    {
+      code: 'ShiftRight',
+      ru: {
+        default: 'shift',
+        shift: 'shift',
+        caps: 'shift',
+      },
+    },
+  ],
+  [
+    {
+      code: 'ControlLeft',
+      ru: {
+        default: 'control',
+        shift: 'control',
+        caps: 'control',
+      },
+    },
+    {
+      code: 'Window',
+      ru: {
+        default: 'Window',
+        shift: 'Window',
+        caps: 'Window',
+      },
+    },
+    {
+      code: 'AltLeft',
+      ru: {
+        default: 'option',
+        shift: 'option',
+        caps: 'option',
+      },
+    },
+    {
+      code: 'Space',
+      ru: {
+        default: ' ',
+        shift: ' ',
+        caps: ' ',
+      },
+    },
+    {
+      code: 'AltRight',
+      ru: {
+        default: 'option',
+        shift: 'option',
+        caps: 'option',
+      },
+    },
+    {
+      code: 'ControlRight',
+      ru: {
+        default: 'control',
+        shift: 'control',
+        caps: 'control',
+      },
+    },
+    {
+      code: 'ArrowLeft',
+      ru: {
+        default: '←',
+        shift: '←',
+        caps: '←',
+      },
+    },
+    {
+      code: 'ArrowUp',
+      ru: {
+        default: '↑',
+        shift: '↑',
+        caps: '↑',
+      },
+    },
+    {
+      code: 'ArrowDown',
+      ru: {
+        default: '↓',
+        shift: '↓',
+        caps: '↓',
+      },
+    },
+    {
+      code: 'ArrowRight',
+      ru: {
+        default: '→',
+        shift: '→',
+        caps: '→',
+      },
+    },
+  ],
+]
+
+const createElement = (arr) => {
+  const body = document.body
+  body.classList.add('first-line')
+  const TITLE = document.createElement('h1')
+  TITLE.classList.add('title')
+  TITLE.textContent = 'RSS Виртуальная клавиатура'
+  body.append(TITLE)
+
+  const TEXTAREA = document.createElement('textarea')
+  TEXTAREA.classList.add('textarea')
+  body.append(TEXTAREA)
+
+  const DIV = document.createElement('div')
+  body.append(DIV)
+}
+createElement(ARR_RU)
